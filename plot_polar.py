f6d5c768499ae060dfdd5927c6661c9dc865fced
Stephanie <shayden8@student.gsu.edu>
2025-12-08 19:34:12 -0500

    pretty acceptable noise

--- plot_polar.py
+++ plot_polar.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-points = np.loadtxt("polar_coords_10001.txt", delimiter=" ")
+points = np.loadtxt("polar_coords_20001.txt", delimiter=" ")
 x, y, z = points[:,0], points[:,1], points[:,2]
 colors = np.loadtxt("noise.txt")
 
@@ -9,7 +9,7 @@ fig = plt.figure()
 
 ax = fig.add_subplot(111, projection="3d")
 
-ax.scatter(x, y, z, c=colors, s=15, cmap='plasma')
+ax.scatter(x, y, z, c=colors, s=15, cmap='viridis')
 
 ax.set_box_aspect([1, 1, 1])
 plt.show()
